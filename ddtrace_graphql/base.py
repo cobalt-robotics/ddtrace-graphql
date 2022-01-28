89bee7b57684a722201ca3a8dc63b22e6903d53e
Mason Bourgeois <mason.bourgeois@gmail.com>
2022-01-27 21:27:27 -0600

    use duck typing rather than inheritance check

--- ddtrace_graphql/base.py
+++ ddtrace_graphql/base.py
@@ -74,45 +74,41 @@ def traced_graphql_wrapped(
                 for key, value in variables.items():
                     span.set_tag(key, value)
 
-
             # `span.error` must be integer
             span.error = int(result is None)
-
-            # Catch the class of cases where result is not an ExecutionResult.
-            # This includes subscription results from graphql-ws, where result
-            # will be an Rx Observable.
-            # TODO: figure out how to trace subscription messages
-            if result is not None and not isinstance(result, graphql.ExecutionResult):
-                return result
 
             if result is not None:
 
                 span.error = 0
-                if result.errors:
-                    span.set_tag(
-                        ERRORS,
-                        utils.format_errors(result.errors))
-                    span.set_tag(
-                        ddtrace_errors.ERROR_STACK,
-                        utils.format_errors_traceback(result.errors))
-                    span.set_tag(
-                        ddtrace_errors.ERROR_MSG,
-                        utils.format_errors_msg(result.errors))
-                    span.set_tag(
-                        ddtrace_errors.ERROR_TYPE,
-                        utils.format_errors_type(result.errors))
+                if hasattr(result, 'errors'):
+                    if result.errors:
+                        span.set_tag(
+                            ERRORS,
+                            utils.format_errors(result.errors))
+                        span.set_tag(
+                            ddtrace_errors.ERROR_STACK,
+                            utils.format_errors_traceback(result.errors))
+                        span.set_tag(
+                            ddtrace_errors.ERROR_MSG,
+                            utils.format_errors_msg(result.errors))
+                        span.set_tag(
+                            ddtrace_errors.ERROR_TYPE,
+                            utils.format_errors_type(result.errors))
 
-                    span.error = int(utils.is_server_error(
-                        result,
-                        ignore_exceptions,
-                    ))
+                        span.error = int(utils.is_server_error(
+                            result,
+                            ignore_exceptions,
+                        ))
 
-                span.set_metric(
-                    CLIENT_ERROR,
-                    int(bool(not span.error and result.errors))
-                )
-                span.set_metric(INVALID, int(result.invalid))
-                span.set_metric(DATA_EMPTY, int(result.data is None))
+                    span.set_metric(
+                        CLIENT_ERROR,
+                        int(bool(not span.error and result.errors))
+                    )
+
+                if hasattr(result, 'invalid'):
+                    span.set_metric(INVALID, int(result.invalid))
+                if hasattr(result, 'data'):
+                    span.set_metric(DATA_EMPTY, int(result.data is None))
 
             if span_callback is not None:
                 span_callback(result=result, span=span)
